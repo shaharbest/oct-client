85b4de9e16162bb7a28d782e8c4c1e77f34fedda
Shahar Best <bestshahar9@gmail.com>
2024-01-14 20:07:45 +0200

    init client side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+
 export default function App() {
-  return 'gimel yafit';
+  return <>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/products' element={<Catalog />} />
+      <Route path='/products/:id' element={<ProductPage />} />
+    </Routes>
+  </>
+}
+
+function  Home() {
+  return 'home';
+}
+
+function  Catalog() {
+  return 'catalog';
+}
+
+function  ProductPage() {
+  return 'product page';
 }
