b37130718ae680593c402c3baacf46eecbc5db13
Shahar Best <bestshahar9@gmail.com>
2024-01-14 19:55:43 +0200

    init firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return 'avi biter';
+  return 'gimel yafit';
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKDgX--sdXC6gePJf2x_RIwSJlIRGOJoo",
+  authDomain: "oct-demo-9a3ab.firebaseapp.com",
+  projectId: "oct-demo-9a3ab",
+  storageBucket: "oct-demo-9a3ab.appspot.com",
+  messagingSenderId: "747839901334",
+  appId: "1:747839901334:web:21140d2e80de089f107cb7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
