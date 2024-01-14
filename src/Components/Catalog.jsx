645b59e5f5bd5d93a9aec7f0a9a5c1c2ffa2f1cd
Shahar Best <bestshahar9@gmail.com>
2024-01-14 21:24:20 +0200

    preparation for pagination

--- src/Components/Catalog.jsx
+++ src/Components/Catalog.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { products } from "../products";
 import ProductCard from "./ProductCard";
 
+const itemsPerPage = 6;
+
 export default function Catalog() {
-  const productsCards = products.map(p =>
-    <ProductCard key={p.id} product={p} />);
+  const [curPageNum, setCurPageNum] = useState(1);
+
+  const firstIndex = (curPageNum - 1) * itemsPerPage;
+  const secondIndex = firstIndex + itemsPerPage;
+
+  const productsCards =
+    products.slice(firstIndex, secondIndex)
+    .map(p => <ProductCard key={p.id} product={p} />);
 
   return <>
     <div className="catalog-board">
