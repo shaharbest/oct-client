9aeb7dab94b5fa1a9ce5d14ba2b9763759cc01c5
Shahar Best <bestshahar9@gmail.com>
2024-01-16 21:33:44 +0200

    search control

--- src/Components/Catalog.jsx
+++ src/Components/Catalog.jsx
@@ -4,14 +4,21 @@ import ProductCard from "./ProductCard";
 import CatalogPagesLinks from "./CatalogPagesLinks";
 
 const itemsPerPage = 6;
-const pagesCount = Math.ceil(products.length / itemsPerPage);
 
 export default function Catalog() {
+  const [searchedProducts, setSearchedProducts] = useState(products);
   const [curPageNum, setCurPageNum] = useState(1);
+  const cards = getPageCards(curPageNum, searchedProducts);
 
-  const cards = getPageCards(curPageNum);
+  function handleSearch(str) {
+    const result = products.filter(p => p.name.includes(str));
+    setSearchedProducts(result);
+  }
+
+  const pagesCount = Math.ceil(searchedProducts.length / itemsPerPage);
 
   return <>
+    <CatalogControls handleSearch={handleSearch} />
     <CatalogBoard cards={cards} />
     <CatalogPagesLinks selectedPageNum={curPageNum}
       onPageBtnClick={num => setCurPageNum(num)}
@@ -19,11 +26,23 @@ export default function Catalog() {
   </>;
 }
 
-function getPageCards(pageNum) {
+function CatalogControls({ handleSearch }) {
+  const [inputVal, setInputVal] = useState('');
+
+  return <div className="catalog-board-controls">
+    <input type="text" value={inputVal}
+      onChange={e => setInputVal(e.target.value)} />
+    <button onClick={() => handleSearch(inputVal)}>
+      search
+    </button>
+  </div>
+}
+
+function getPageCards(pageNum, allProducts) {
   const firstproductIndex =
     (pageNum - 1) * itemsPerPage;
-  
-  const pageProducts = products.slice(firstproductIndex,
+
+  const pageProducts = allProducts.slice(firstproductIndex,
     firstproductIndex + itemsPerPage)
 
   return pageProducts.map(p =>
