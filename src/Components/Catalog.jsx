6f6811ebb3bb9c03d2b00a5dda2ae6a009d958a7
Shahar Best <bestshahar9@gmail.com>
2024-01-16 22:06:50 +0200

    sort feature

--- src/Components/Catalog.jsx
+++ src/Components/Catalog.jsx
@@ -5,20 +5,40 @@ import CatalogPagesLinks from "./CatalogPagesLinks";
 
 const itemsPerPage = 6;
 
+const sortCategories = ['name', 'price'];
+
 export default function Catalog() {
   const [searchedProducts, setSearchedProducts] = useState(products);
   const [curPageNum, setCurPageNum] = useState(1);
   const cards = getPageCards(curPageNum, searchedProducts);
 
   function handleSearch(str) {
-    const result = products.filter(p => p.name.includes(str));
+    const result = products.filter(p =>
+      p.name.includes(str));
     setSearchedProducts(result);
+  }
+
+  function handleSort(category) {
+    const productsClone = [...products];
+
+    switch (category) {
+      case 'name':
+        productsClone.sort((p1, p2) => p1.name.localeCompare(p2.name));
+        break;
+      case 'price':
+        productsClone.sort((p1, p2) => p1.price - p2.price);
+        break;
+    }
+
+    setSearchedProducts(productsClone);
   }
 
   const pagesCount = Math.ceil(searchedProducts.length / itemsPerPage);
 
   return <>
-    <CatalogControls handleSearch={handleSearch} />
+    <CatalogControls
+      handleSort={handleSort}
+      handleSearch={handleSearch} />
     <CatalogBoard cards={cards} />
     <CatalogPagesLinks selectedPageNum={curPageNum}
       onPageBtnClick={num => setCurPageNum(num)}
@@ -26,8 +46,13 @@ export default function Catalog() {
   </>;
 }
 
-function CatalogControls({ handleSearch }) {
+function CatalogControls({ handleSearch, handleSort }) {
   const [inputVal, setInputVal] = useState('');
+  
+  const sortOptions = ['', ...sortCategories]
+    .map(c => <option key={c} value={c}>
+      {c}
+    </option>);
 
   return <div className="catalog-board-controls">
     <input type="text" value={inputVal}
@@ -35,6 +60,9 @@ function CatalogControls({ handleSearch }) {
     <button onClick={() => handleSearch(inputVal)}>
       search
     </button>
+    <select onChange={e => handleSort(e.target.value)}>
+      {sortOptions}
+    </select>
   </div>
 }
 
