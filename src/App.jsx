0275c2c9e7ada34ba08020cd8e8c6717272eeef5
Shahar Best <bestshahar9@gmail.com>
2024-01-30 21:44:00 +0200

    lots of stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Catalog from "./Components/Catalog";
+import Users from "./Components/Users";
 import ProductPage from "./Components/ProductPage";
 import Navbar from "./Components/Navbar";
 import LocalSorageDemo from "./Components/LocalStorageDemo";
+
 
 export default function App() {
   return <>
@@ -13,6 +15,7 @@ export default function App() {
       <Route path='/products' element={<Catalog />} />
       <Route path='/products/:id' element={<ProductPage />} />
       <Route path='/storage' element={<LocalSorageDemo />} />
+      <Route path='/users' element={<Users />} />
     </Routes>
   </>;
 }
